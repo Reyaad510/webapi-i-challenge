029acf2ef9fc5b75bb6d39be7aefb644125a09a8
Reyaad Rafeek <reyaad510@gmail.com>
2019-05-08 13:16:09 -0400

    Added require express and listening port

--- index.js
+++ index.js
@@ -1,1 +1,12 @@
 // implement your API here, initial commit
+
+const express = require('express');
+const server = express();
+
+
+
+// Listening
+
+server.listen(1234, () => {
+    console.log('Listening on port 1234')
+});
